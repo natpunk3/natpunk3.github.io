cb2b08316913a1833bb43ecbe3d4e3c669c9ae53
Natpunk3 <122226510+natpunk3@users.noreply.github.com>
2026-09-03 20:42:51 -0500

    Update mirrormake.sh

--- archlinux/mirrormake.sh
+++ archlinux/mirrormake.sh
@@ -5,6 +5,6 @@
 SERV_URL="https://mirrors.ocf.berkely.edu/archlinux/"
 echo "Configuring Mirrorlist..."
 echo "Setting to $SERV_URL ..."
-echo "Server = $SERV_URL/\$repo/os/\$arch" >> /tmp/pacman.d.mirrorlist
+echo "Server = $SERV_URL/\$repo/os/\$arch" > /tmp/pacman.d.mirrorlist
 echo "Configured Mirrorlist sending to /etc/pacman.d/"
 mv /tmp/pacman.d.mirrorlist /etc/pacman.d/mirrorlist
